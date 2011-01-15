97df69d1a2df698f74dbf5d30b02c6efb4e537c3
Kasper F. Brandt <poizan@poizan.dk>
2011-01-15 06:06:12 +0100

    Some unfinished work with functions "returning" values in pointer arguments

--- codegen.py
+++ codegen.py
@@ -48,54 +48,75 @@ while i < len(lines):
 			while re.search('#endif', lines[i]) == None:
 				i+=1
 		m = re.search('^\s*(\w(\w|\s)+?\*?)\s+\(\*(\w+)\)\s*\((.*)\);.*$', lines[i])
-		if m != None:
-			ret = m.group(1)
-			name = m.group(3)
-			args = [[x.strip() for x in aDec.split()] for aDec in m.group(4).split(',')]
-			args = [(" ".join(x[:-1]), x[-1:][0]) for x in args]
-			if args == [('', 'void')]:
-				args = []
-			print 'struct="%s", ret="%s", name="%s", args=%s' % (struct,ret,name,args)
-			enumName = struct+'_'+name
-			enum.write(enumName+',\n')
-			try:
-				nargs = [(ntypes[arg[0]], arg[1]) for arg in args]
-				if ret != 'void':
-					nret = ntypes[ret]
-			except KeyError as e:
-				print "Unknown type: "+e.message+". Not generating code!"
+		if m == None:
+			i+=1
+			continue
+		ret = m.group(1)
+		name = m.group(3)
+		args = [[x.strip() for x in aDec.split()] for aDec in m.group(4).split(',')]
+		args = [(" ".join(x[:-1]), x[-1:][0]) for x in args]
+		if args == [('', 'void')]:
+			args = []
+		print 'struct="%s", ret="%s", name="%s", args=%s' % (struct,ret,name,args)
+		enumName = struct+'_'+name
+		enum.write(enumName+',\n')
+		nins = []
+		nouts = []
+		nargs = []
+		for arg in args:
+			if ntypes.has_key(arg[0]):
+				nins.append((ntypes[arg[0]], arg[1]))
+				nargs.append((ntypes[arg[0]], arg[1], "in"))
+			elif arg[0][-1:] == '*' and ntypes.has_key(arg[0][:-1]):
+				nouts.append((ntypes[arg[0][:-1]], arg[1]))
+				nargs.append((ntypes[arg[0][:-1]], arg[1], "out"))
+			else:
+				print 'Unknown argument type: "'+arg[0]+'" for argument "'+arg[1]+'". Not generating code!'
 				i+=1
 				continue
-			jargs = [(jtypes[arg[0][0]], arg[1]) for arg in nargs]
+		try:
 			if ret != 'void':
-				jret = jtypes[nret[0]]
-			nc  = "      case ClientCommand::"+enumName+":\n"
-			nc += "      {\n"
-			for narg in nargs:
-			#int32_t type = readInt32();
-				nc  += "        %s %s = read%s();\n" % (narg[0][0], narg[1], narg[0][1])
-			ncc = ", ".join([narg[0][2](narg[1]) for narg in nargs])
-			retdec = ""
-			if ret != 'void':
-				retdec = nret[0]+" retval = "
-			nc += "        "+retdec+"ms->"+struct+"."+name+"("+ncc+");\n"
-			nc += "        write"+nret[1]+"(retval);\n"
-			nc += "        break;\n"
-			nc += "      }\n"
-			ncode.write(nc)
-			jdec = ", ".join([jarg[0][0]+" "+jarg[1] for jarg in jargs])
-			if ret != 'void':
-				jretType = jret[0]
-			else:
-				jretType = 'void'
-			jc  =  "	public "+jretType+" "+enumName+"("+jdec+")\n"
-			jc +=  "	{\n"
-			jc +=  "		writeInt(ClientCommand."+enumName+".ordinal());\n"
-			for jarg in jargs:
-				jc +=  "		write"+jarg[0][1]+"("+jarg[1]+");\n"
-			jc +=  "		flush();\n"
-			if ret != 'void':
-				jc +=  "		return read"+jret[1]+"();\n"
-			jc +=  "	}\n"
-			jcode.write(jc)
+				nret = ntypes[ret]
+		except KeyError as e:
+			print 'Unknown return type: "'+ret+'". Not generating code!'
+			i+=1
+			continue
+		jins = [(jtypes[arg[0][0]], arg[1]) for arg in nins]
+		jouts = [(jtypes[arg[0][0]], arg[1]) for arg in nouts]
+		jargs = [(jtypes[arg[0][0]], arg[1]) for arg in nargs]
+		if ret != 'void':
+			jret = jtypes[nret[0]]
+		nc  = "      case ClientCommand::"+enumName+":\n"
+		nc += "      {\n"
+		for narg in nins:
+		#int32_t type = readInt32();
+			nc  += "        %s %s = read%s();\n" % (narg[0][0], narg[1], narg[0][1])
+		for narg in nouts:
+			nc  += "        %s %s;\n" % (narg[0][0], narg[1])
+		ncc = ", ".join([narg[0][2](narg[1]) for narg in nargs])
+		retdec = ""
+		if ret != 'void':
+			retdec = nret[0]+" retval = "
+		nc += "        "+retdec+"ms->"+struct+"."+name+"("+ncc+");\n"
+		for narg in nouts:
+			nc += "        write%s(%s);\n" % (narg[0][1], narg[1])
+		nc += "        write"+nret[1]+"(retval);\n"
+		nc += "        break;\n"
+		nc += "      }\n"
+		ncode.write(nc)
+		jdec = ", ".join([jarg[0][0]+" "+jarg[1] for jarg in jargs])
+		if ret != 'void':
+			jretType = jret[0]
+		else:
+			jretType = 'void'
+		jc  =  "	public "+jretType+" "+enumName+"("+jdec+")\n"
+		jc +=  "	{\n"
+		jc +=  "		writeInt(ClientCommand."+enumName+".ordinal());\n"
+		for jarg in jargs:
+			jc +=  "		write"+jarg[0][1]+"("+jarg[1]+");\n"
+		jc +=  "		flush();\n"
+		if ret != 'void':
+			jc +=  "		return read"+jret[1]+"();\n"
+		jc +=  "	}\n\n"
+		jcode.write(jc)
 		i+=1
